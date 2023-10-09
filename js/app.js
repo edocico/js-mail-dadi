4372517d5bc8f45c8896daed39a970026a74c5e5
edocico <138378767+edocico@users.noreply.github.com>
2023-10-09 18:13:06 +0200

    add email verification

--- js/app.js
+++ js/app.js
@@ -8,6 +8,12 @@ const sendButtonDOMElement = document.getElementById('send-button')
 
 const inputEmailDOMElement = document.querySelector('.input-email')
 
+const mainDOMElement = document.getElementById('main')
+
+const pDOMElement = document.getElementById('access-p')
+
+const playBtnDOMElement = document.getElementById('play-button')
+
 // creo un array di email
 
 const emailArray = [
@@ -23,7 +29,7 @@ console.log(emailArray)
 
 // dichiaro una variabile per salvare il valore dell'array length
 
-let arrayLength = emailArray.length
+const arrayLength = emailArray.length
 
 console.log(arrayLength)
 
@@ -36,9 +42,43 @@ sendButtonDOMElement.addEventListener('click', function() {
 
     // pusho il valore recuperato nell'array
 
-    emailArray.push(userEmail)
+    //emailArray.push(userEmail)
+
+    // dichiaro variabile accesso a cui assegno valore false di default
+
+    let accesso = false
 
     console.log(emailArray)
 
-    console.log(arrayLength)
+    console.log(arrayLength, emailArray.length)
+
+    // ciclo l'array
+
+    for (let i = 0; i < arrayLength; i++) {
+        
+        // dichiaro variabile mail per salvare il valore dell'index dell'array
+        let mail = emailArray[i]
+        console.log(mail)
+
+        // con la condizionale se a una delle email presenti nell'array corrisponde la email inserita dall'utente
+        if (userEmail === mail) {
+            // se ne trovo una la variabile accesso assume il valore true
+            accesso = true
+        }
+    }
+    // se la variabile accesso assume il valore true allora l'accesso è consentito altrimenti no
+    if ( accesso === true) {
+
+        pDOMElement.innerHTML = 'accesso consentito'
+        
+    } else {
+        pDOMElement.innerHTML = 'accesso negato'
+    }
+
+    if (accesso === true) {
+        playBtnDOMElement.addEventListener('click', function() {
+
+        })
+    }
+         
 })
